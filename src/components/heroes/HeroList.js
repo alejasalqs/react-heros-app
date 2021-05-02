f849f8a35c78dd00c43898f17b35ed670e879b78
Alejandro Salguero <alejandrosalgueroq@hotmail.com>
2021-05-02 12:25:35 -0600

    Diseno vista heroes

--- src/components/heroes/HeroList.js
+++ src/components/heroes/HeroList.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { getHeroByPublisher } from "../../selectos/getHeroesByPublisher";
+import { HeroCard } from "./HeroCard";
 
 // por props enviamos el publisher con el cual filtramos dc o marvel
+// de esta manera {...hero} se envia un objeto con las propiedades separadas a los prop
 export const HeroList = ({ publisher }) => {
   const heros = getHeroByPublisher(publisher);
   return (
-    <ul>
+    <div className="card-columns">
       {heros.map((hero) => (
-        <li key={hero.id}>{hero.superhero}</li>
+        <HeroCard key={hero.id} {...hero} />
       ))}
-    </ul>
+    </div>
   );
 };
