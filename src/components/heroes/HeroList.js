c8c64b18fe932ceb675bb05beca2b77f5219c9b4
Alejandro Salguero <alejandrosalgueroq@hotmail.com>
2021-05-02 12:59:28 -0600

    Mejoras CSS

--- src/components/heroes/HeroList.js
+++ src/components/heroes/HeroList.js
@@ -8,7 +8,7 @@ export const HeroList = ({ publisher }) => {
   //const heros = getHeroByPublisher(publisher);
   const heros = useMemo(() => getHeroByPublisher(publisher), [publisher]);
   return (
-    <div className="card-columns">
+    <div className="card-columns animate__animated animate__fadeIn">
       {heros.map((hero) => (
         <HeroCard key={hero.id} {...hero} />
       ))}
