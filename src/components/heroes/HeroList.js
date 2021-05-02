7fe0f9ba59235840818a2abf95e74779872d5a5a
Alejandro Salguero <alejandrosalgueroq@hotmail.com>
2021-05-02 12:55:29 -0600

    Mejoras en html y useMemo

--- src/components/heroes/HeroList.js
+++ src/components/heroes/HeroList.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { getHeroByPublisher } from "../../selectos/getHeroesByPublisher";
 import { HeroCard } from "./HeroCard";
 
 // por props enviamos el publisher con el cual filtramos dc o marvel
 // de esta manera {...hero} se envia un objeto con las propiedades separadas a los prop
 export const HeroList = ({ publisher }) => {
-  const heros = getHeroByPublisher(publisher);
+  //const heros = getHeroByPublisher(publisher);
+  const heros = useMemo(() => getHeroByPublisher(publisher), [publisher]);
   return (
     <div className="card-columns">
       {heros.map((hero) => (
--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,9 +1,64 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { Redirect, useParams } from "react-router";
+import { getHeroById } from "../../selectos/getHeroByID";
 
-export const HeroScreen = () => {
+export const HeroScreen = ({ history }) => {
+  // Hook de react.router que nos permite obtener los paramentros del url
+  // retorna un objeto por lo que se puede desestructurar
+  const { heroId } = useParams();
+
+  //const hero = getHeroById(heroId);
+  const hero = useMemo(() => getHeroById(heroId), [heroId]);
+
+  if (!hero) {
+    // Como buena practica un componente siempre deve devolver algo que no sea un error
+    // En este caso si el heroes no existe nos devuleve un redirect a otra pagina
+    return <Redirect to="/marvel" />;
+  }
+
+  const {
+    superhero,
+    alter_ego,
+    first_appearance,
+    characters,
+    publisher,
+  } = hero;
+
+  const handleReturn = () => {
+    history.goBack();
+  };
+
   return (
-    <div>
-      <h1>Heros</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={`../Assets/heroes/${heroId}.jpg`}
+          className="img-thumbnail"
+          alt={superhero}
+        />
+      </div>
+      <div className="col-8">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego: </b>
+            {alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher: </b>
+            {publisher}
+          </li>
+          <li className="list-group-item">
+            <b>Firts apperance: </b>
+            {first_appearance}
+          </li>
+        </ul>
+        <h5>Characters:</h5>
+        <p>{characters}</p>
+        <button className="btn btn-outline-info" onClick={handleReturn}>
+          Return
+        </button>
+      </div>
     </div>
   );
 };
