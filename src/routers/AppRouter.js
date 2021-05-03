f94f89e218d362479384934a39f745a59b0bf135
Alejandro Salguero <alejandrosalgueroq@hotmail.com>
2021-05-03 09:59:39 -0600

    Rutas publicas y privadaas

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { AuthContext } from "../auth/AuthContext";
 import { LoginScreen } from "../components/login/LoginScreen";
 import { DashboardRoutes } from "./DashboardRoutes";
+import { PrivateRoutes } from "./PrivateRoutes";
+import { PublicRoutes } from "./PublicRoutes";
 // Sistema de rutas principal de la aplicacion
 export const AppRouter = () => {
+  const { user } = useContext(AuthContext);
   return (
     <Router>
       <>
         <Switch>
-          <Route exact path="/login" component={LoginScreen} />
+          <PublicRoutes
+            exact
+            path="/login"
+            component={LoginScreen}
+            isAuthenticated={user.logged}
+          />
 
           {/*En este caso no es necesario el exact por las rutas que estan abajo de esta*/}
-          <Route path="/" component={DashboardRoutes} />
+          <PrivateRoutes
+            path="/"
+            component={DashboardRoutes}
+            isAuthenticated={user.logged}
+          />
         </Switch>
       </>
     </Router>
---
+++ src/routers/PrivateRoutes.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Redirect, Route } from "react-router";
+
+export const PrivateRoutes = ({
+  isAuthenticated,
+  component: Component, // Se renombre la variable component por Component
+  ...rest // Los demas elementos
+}) => {
+  // Lo que hace aca es retornar una ruta, la cual tiene como componente el que se envia por parametros
+  // El componente que se va a mostrar se le envia por parametros las props
+  // Se verifica que el usuario este autenticado para decidir si se muestra o no
+  return (
+    <Route
+      {...rest}
+      component={(props) =>
+        isAuthenticated ? <Component {...props} /> : <Redirect to="/login" />
+      }
+    />
+  );
+};
+
+PrivateRoutes.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
+  component: PropTypes.func.isRequired,
+};
