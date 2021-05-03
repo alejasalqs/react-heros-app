b7a35ed35cccec00ed1514ea59d001ee319a87ce
Alejandro Salguero <alejandrosalgueroq@hotmail.com>
2021-05-03 09:34:14 -0600

    Login y Logout

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
 import { AuthContext } from "./auth/AuthContext";
 import { authReducer } from "./auth/authReducer";
 import { AppRouter } from "./routers/AppRouter";
@@ -16,6 +16,11 @@ const init = () => {
 
 export const HeroesApp = () => {
   const [user, dispatch] = useReducer(authReducer, {}, init);
+
+  useEffect(() => {
+    // Cada vez que cambia el usuario guarda en localStorage
+    localStorage.setItem("user", JSON.stringify(user));
+  }, [user]);
 
   return (
     <AuthContext.Provider value={{ user, dispatch }}>
--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useContext, useReducer } from "react";
+import { AuthContext } from "../../auth/AuthContext";
+import { authReducer } from "../../auth/authReducer";
+import { types } from "../../types/types";
 
 export const LoginScreen = ({ history }) => {
   // props history lo tenemos gracias a react router
+
+  const { dispatch } = useContext(AuthContext);
+
   const handleLogin = () => {
     // el push nos redirecciona a otra pagina usando react router
     //history.push("/");
     // el replace lo que hace es reemplazar el historail, lo cual evitar usar el boton de atras
+    dispatch({
+      type: types.login,
+      payload: {
+        name: "Alejandro",
+      },
+    });
     history.replace("/");
   };
   return (
